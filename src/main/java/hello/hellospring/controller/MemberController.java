ba14d0453370a26a94614b6c7bf1f569c2b10794
seolkd <seolkd.e@gmail.com>
2022-01-30 16:46:32 +0900

    자바 코드로 직접 스프링 빈 등록하기 (Java Config)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,10 +7,27 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
+    //생성자 주입
     private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    //필드주입 (스프링이 권장하지 않음, 변경할수가 없음)
+    /*
+    @Autowired
+    private MemberService memberService;
+    */
+
+    //Setter 주입(누군가가 호출했을 때 public이 열려 있어야 한다. 노출이 된다. 잘못바꾸면 문제가 생김)
+    /*
+    private MemberService memberService;
+
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+    */
 }
