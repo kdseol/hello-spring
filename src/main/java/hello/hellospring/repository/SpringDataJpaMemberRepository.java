a5a83438bb61b2de732943f4eac612a06e2efc82
seolkd <seolkd.e@gmail.com>
2022-01-31 14:17:54 +0900

    스프링 데이터 JPA

---
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    //JPQL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
+    //findByNameandId -> 단순한 80%는 인터페이스 이름으로 개발이 끝낼 수 있다.
+}
